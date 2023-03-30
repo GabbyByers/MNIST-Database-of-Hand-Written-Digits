32ac6933ac8871f454f0cd7815d443ccfed9d6a7
Gabbie <81900622+GabrielBuckingham@users.noreply.github.com>
2023-03-29 17:49:52 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "SFML/Graphics.hpp"
 
 using namespace std;
+#define _CRTDBG_MAP_ALLOC
 
 class matrix
 {
@@ -25,6 +27,10 @@ public:
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Hello SFML", sf::Style::Close);
+	sf::Event event;
+
 	ifstream file;
 	file.open("IDX/test-images.idx3-ubyte", ios::binary);
 
@@ -43,17 +49,31 @@ int main()
 	}
 
 	cout << "The file has " << bytes.size() << " bytes.\n\n";
-	
+
 	for (int i = 0; i < 16; i++)
 	{
 		cout << (unsigned)bytes[i] << "\n";
-		if ((i+1) % 4 == 0) { cout << "\n"; }
+		if ((i + 1) % 4 == 0) { cout << "\n"; }
 	}
 
 	int* head = reinterpret_cast<int*>(&bytes[0]);
 	for (int* ptr = head; ptr < head + 4; ptr++)
 	{
 		cout << *ptr << "\n";
+	}
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear(sf::Color(0, 0, 0));
+		window.display();
 	}
 
 	return 0;
